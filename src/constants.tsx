0166f2659fed7164401f12f68866734386775769
Juliette Pretot <hi@juliette.sh>
2018-12-15 22:48:27 +0100

    mark constants as readonly

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,4 +1,4 @@
-export const FONTS_URLS = {
+export const FONTS_URLS: DeepReadonly<{ [s: string]: string }> = {
   regular: '/fonts/jregular.woff2',
   bold: '/fonts/jbold.woff2',
   sans: '/fonts/jsans.woff2'
